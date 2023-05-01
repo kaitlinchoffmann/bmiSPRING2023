22dc9c02e9b4c4a5d4450072ba99204ebcb70778
kaitlinhoffmann <kaitlinchoffmann@gmail.com>
2023-05-01 14:02:40 -0400

    Create and Read on User

--- server/models/user.js
+++ server/models/user.js
@@ -1,41 +1,95 @@
-const users = [
-  {
-   userId: 134,
-   userName: "cathy123",
-   password: "icecream"  
-  },
-  {
-    userId: 534,
-    userName: "bill123",
-    password: "badpassword"  
-   },
-   {
-    userId: 654,
-    userName: "jess",
-    password: "password"  
-   }
-]
+const con = require("./db_connect");
 
-// functions to complete CRUD operations
-function getAllUsers() {
-  return users;
+async function createTable() {
+  let sql = `CREATE TABLE IF NOT EXISTS users (
+    userID INT NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(255) NOT NULL UNIQUE,
+    userWeight NUMERIC,
+    userHeight NUMERIC,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT userPK PRIMARY KEY(userID)
+  );`
+
+  await con.query(sql);  
 }
 
-function login(user) {
-  /* user {
-    userName: "cathy123",
-    password: "icecream"
-  } */
-  let cUser = users.filter(u => u.userName == user.userName);
-  if(!cUser[0]) throw Error("Username does not exist!");
+createTable();
 
+// functions to complete CRUD operations
+async function getAllUsers() {
+  let sql = `SELECT * FROM users;`
+  return await con.query(sql);
+}
+
+async function userExists(userName) {
+  let sql = `
+    SELECT * FROM users
+    WHERE userName = "${userName}"
+  `
+  return await con.query(sql);
+}
+
+async function login(user) {
+  let cUser = await userExists(user.userName);
+  if(!cUser[0]) throw Error("Username does not exist!");
   if(cUser[0].password != user.password) throw Error("Password is incorrect!");
 
   return cUser[0];
 }
 
-function randomFunction() {
-  return "Hello World!";
+async function register(user) {
+  // check to see if username is already in use
+  let cUser = await userExists(user.userName);
+  if(cUser.length>0) throw Error("Username already exists");
+
+  // create new user
+  let sql = `
+    INSERT INTO users(userName, password)
+    VALUES ("${user.userName}", "${user.password}");
+  `
+  await con.query(sql)
+
+  // get user to send over
+  cUser = await getUser(user)
+  console.log(cUser)
+  return cUser[0];
 }
 
-module.exports = { getAllUsers, randomFunction, login }
+async function getUser(user) {
+  let sql;
+  if(user.userID) {
+    sql = `
+      SELECT * FROM users
+      WHERE userID = ${user.userID};
+    `
+  } else {
+    sql = `
+      SELECT * FROM users
+      WHERE userName = "${user.userName}";
+    `
+  }
+
+  return con.query(sql);
+}
+
+module.exports = { getAllUsers, login, register }
+
+
+
+// const users = [
+//   {
+//    userId: 134,
+//    userName: "cathy123",
+//    password: "icecream"  
+//   },
+//   {
+//     userId: 534,
+//     userName: "bill123",
+//     password: "badpassword"  
+//    },
+//    {
+//     userId: 654,
+//     userName: "jess",
+//     password: "password"  
+//    }
+// ]
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ const router = express.Router()
 router.get('/', async (req, res) => {
   try {
     const users = await User.getAllUsers();
+    console.log(users)
     res.send(users); 
   } catch(err) {
     res.status(401).send({message: err.message})
@@ -14,6 +15,15 @@ router.get('/', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     let user = await User.login(req.body); // {userName: "cathy123", password: "icecream"}
+    res.send({...user, password: undefined})
+  } catch(err) {
+    res.status(401).send({message: err.message}) // "Username not found"
+  }
+})
+
+router.post('/register', async(req, res) => {
+  try {
+    let user = await User.register(req.body);
     res.send({...user, password: undefined})
   } catch(err) {
     res.status(401).send({message: err.message}) // "Username not found"
